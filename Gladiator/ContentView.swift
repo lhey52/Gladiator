d3ecc5bee637bad251f8b8fb8c47743a9909f66c
Daniel Hey <l.hey52@aol.com>
2026-03-17 16:57:45 -0400

    Initial Commit

---
+++ Gladiator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gladiator
+//
+//  Created by Daniel Hey on 3/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gladiator/GladiatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  GladiatorApp.swift
+//  Gladiator
+//
+//  Created by Daniel Hey on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct GladiatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
